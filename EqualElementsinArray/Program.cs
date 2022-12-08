112c742bf2a6afc6a4be8c6ad444874044e0d0e7
ArtemLevin <python_study_2022@mail.ru>
2022-12-08 22:55:39 +0300

    TicTac Winner Bug

---
+++ EqualElementsinArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {1, 3, 4, 1, 5, 6, 6, 1};
+int len = array.Length;
+int j, i;
+
+for (i=0; i<len; i++)
+{
+    for (j = i+1; j<len; j++)
+    {
+        if (array[j] ==  array[i])
+        {
+            Console.WriteLine ($"Element = {array[i]}, index = {i}, index = {j}");
+        }
+    }
+}
